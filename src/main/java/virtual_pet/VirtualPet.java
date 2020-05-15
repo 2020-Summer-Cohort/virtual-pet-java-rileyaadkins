17d25d384e6074578bd9fd6b94e1e11cb1c65dca
Riley Adkins <rileyaadkins@gmail.com>
2020-05-15 15:16:02 -0400

    Virtual Pet EOD 5/15

--- src/main/java/virtual_pet/VirtualPet.java
+++ src/main/java/virtual_pet/VirtualPet.java
@@ -1,4 +1,30 @@
 package virtual_pet;
 
+import java.util.Scanner;
+
 public class VirtualPet {
+//   void tick(){
+
+    public int headPet;
+    public int buttPet;
+    public int giveTreat;
+    public int scoopPoop;
+
+    public VirtualPet (int headPet, int buttPet, int giveTreat, int scoopPoop){
+        this.headPet = headPet;
+        this.buttPet = buttPet;
+        this.giveTreat = giveTreat;
+        this.scoopPoop = scoopPoop;
+    }
+
+    public int giveHeadPet(){return headPet;}
+
+    public int giveButtPet(){return buttPet;}
+
+    public int giveTreat(){return giveTreat;}
+
+    public int scoopPoop(){return scoopPoop;}
+
+
+
 }
--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -1,9 +1,28 @@
 package virtual_pet;
 
+import java.util.Scanner;
+
 public class VirtualPetApplication {
 
     public static void main(String[] args) {
-        //Interact with a VirtualPet object in this method
+        Scanner input = new Scanner(System.in);
+
+        VirtualPet scullyTheCat = new VirtualPet(4,6,3,0);
+
+        System.out.println("Welcome to your Virtual Pet!");
+        System.out.println("This is Scully 龴ↀ◡ↀ龴");
+        System.out.println("Head Pets: " + scullyTheCat.giveHeadPet());
+        System.out.println("Butt Pets: " + scullyTheCat.giveButtPet());
+        System.out.println("Give Treats: " + scullyTheCat.giveTreat());
+        System.out.println("Scoop Poop: " + scullyTheCat.scoopPoop());
+
+
+
+
+
+        while (true) {
+
+        }
     }
 
 }
