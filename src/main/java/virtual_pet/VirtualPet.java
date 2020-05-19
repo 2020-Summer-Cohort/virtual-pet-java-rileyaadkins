3b03eb278c48c2b361a910936348be2c5fc27f3c
Ben Williams <DeanPBR@gmail.com>
2020-05-18 23:27:44 -0400

    Adding grading feedback

--- src/main/java/virtual_pet/VirtualPet.java
+++ src/main/java/virtual_pet/VirtualPet.java
@@ -1,7 +1,18 @@
 package virtual_pet;
 
 import java.util.Random;
-
+/*
+ * I would like to see some improvements to this application:
+ * 1 - Rename methods that are 'getters' to 'get' + the field name to act as getters.
+ * 2 - If you want to give a 'headPet' you should have a method called 'petHead()' or something similar.  These methods
+ *     will be how you change the amount of pets your pet has.
+ * 3 - Remove the static keyword from the field declarations.  It's unneeded and will produce errors when we build on
+ *     this project.
+ * 4 - You can move the petLevels method to the VirtualPetApplication class to keep things that use the console in the
+ *     Application class.  All the pet should care about is the pet things, not writing to the console.
+ * 5 - Add a tick() method to turn this into a turn based game, when the user makes an action maybe the pet's hunger
+ *     goes up and the need to poop goes up.  Maybe it's pet count starts to diminish.
+ */
 public class VirtualPet {
     Random random = new Random();
 
